881f60f7541b5f84d895941eea3dd4aa40178180
Joe <iam3djoe@gmail.com>
2026-04-18 15:11:23 +0100

    hex selection

--- Assets/HexGrid.cs
+++ Assets/HexGrid.cs
@@ -1,17 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HexGrid : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask _hexLayer;
+
+    [SerializeField]
+    private Transform _highlightObj;
+
+    private MapHex _currentHex;
+
+    private Camera _cam;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray worldRay = _cam.ScreenPointToRay(Mouse.current.position.value);
+        if (Physics.Raycast(worldRay, out RaycastHit hit, 999, _hexLayer))
+        {
+            if (hit.collider.gameObject.TryGetComponent(out MapHex hex))
+            {
+                _currentHex = hex;
+                _highlightObj.position = _currentHex.transform.position;
+            }
+        }
     }
 
     [ContextMenu("SYNC ALL")]
